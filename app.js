1db268bafe011c0bfef47f2dcc4d2f3e97da8b8f
Felix <felix.j.yu@gmail.com>
2020-12-26 21:34:24 -0500

    built a simple api

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.0</p>');
+})
+
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
+})
+
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
+})
---
+++ scripts/application_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/express-app
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+#install node modules
+npm install
+
+#start our node app in the background
+node app.js > app.out.log 2> app.err.log < /dev/null & 
