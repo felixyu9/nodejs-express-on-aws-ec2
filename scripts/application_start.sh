d5af1822c05c20ce7181eae373ba889feab3543a
Felix <felix.j.yu@gmail.com>
2020-12-28 15:07:03 -0500

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -9,7 +9,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-npm99 install
+npm install
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
