7ea5134b23d9ac465e3a07ee5fd001f237c7d49a
Muqtar <88159236+Muqtarmav@users.noreply.github.com>
2022-03-03 11:30:53 +0100

    new update
    new product

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 4</p>');
 })
 
 app.get('/products', (req, res) => {
@@ -16,6 +16,11 @@ app.get('/products', (req, res) => {
       productId: '102',
       price: 150
     }
+     {
+      productId: '103',
+      price: 200
+    }
+
   ])
 })
 
