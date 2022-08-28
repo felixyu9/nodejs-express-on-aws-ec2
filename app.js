a3a10f6348262208f3dc67af1bd3331cef76e4ff
Amit Kumar <amit.active2008@gmail.com>
2022-08-28 17:06:46 +0530

    test1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.2</p>');
 })
 
 app.get('/products', (req, res) => {
